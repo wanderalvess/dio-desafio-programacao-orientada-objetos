192ee7ac11783a13e19b9f962945ad73fec860f7
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2022-02-06 17:04:26 -0300

    Commit das classes

---
+++ Desafio dominio/src/Main.java
@@ -0,0 +1,31 @@
+import desafio.dominio.Conteudo;
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição java curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("descrição js curso");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria Java");
+        mentoria1.setDescricao("mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+
+    }
+}
+
